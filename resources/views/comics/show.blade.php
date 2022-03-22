e56145e5e92ea58f1aac66436ade5375383dc9b8
SangesAntonio <sanges65@gmail.com>
2022-03-22 16:12:12 +0100

    added show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,15 +1,12 @@
 @extends('layout/main')
 
 @section('content')
-@foreach ($comics as $comic)
+
 <div class="card" style="width: 18rem;">
     <img src="{{$comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
     <div class="card-body">
       <h5 class="card-title">{{$comic['title']}}</h5>
       <p class="card-text">{{$comic['description']}}</p>
-      <a href="{{route('comics.show', $comic->id)}}" class="btn btn-primary">Go to the comic book</a>
     </div>
   </div>
-
-  @endforeach
 @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout/main')
+
+@section('content')
+<h1>Non ci sono comics</h1>
+
+@endsection
